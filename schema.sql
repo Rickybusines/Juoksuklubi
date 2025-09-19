bcf8315b43f622d50d08e57c8c7876110929998f
Rickybusines <mr.lindd@gmail.com>
2025-09-19 15:10:52 +0300

    Add new item form

--- schema.sql
+++ schema.sql
@@ -3,3 +3,12 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE items(
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    description TEXT,
+    pace INTEGER,
+    length INTEGER,
+    user_id INTEGER REFERENCES users
+);
